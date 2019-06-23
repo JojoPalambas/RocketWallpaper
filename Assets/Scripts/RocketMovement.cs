8ac8f5d56282d38f1b9a09bb5d9d99854886b927
Gabriel Depotte <depott_g@epita.fr>
2019-06-23 18:41:27 +0200

    [unity] Added a sin movement

---
+++ Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+    public float curveAmplitude;
+    public float curveFrequency;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.time * curveFrequency) * curveAmplitude, transform.position.z);
+    }
+}
